e4d29deffccd620fe05cbd40a97a5804a0d81129
mahdiidarabi <30820569+mahdiidarabi@users.noreply.github.com>
2018-09-05 16:30:47 +0430

    Update greeter.sol

--- Intro to Solidity/contracts/greeter.sol
+++ Intro to Solidity/contracts/greeter.sol
@@ -1,12 +1,26 @@
-pragma solidity 0.4.19;
+pragma solidity ^0.4.19;
 
 
 contract Greeter {
     string private greeting;
+    address private owner;
 
     function Greeter(string _greeter) public {
+        owner = msg.sender;
+        greeting = _greeter;
     }
 
     function greet() public view returns (string) {
+        return greeting;
+    }
+    
+    function change(string _greeter) public returns(bool){
+        if(msg.sender == owner){
+            greeting = _greeter;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
